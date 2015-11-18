4fd8f97606e19ada5b98117b5de7378695d1dd0d
david.pate <david@davidpate.com>
2015-11-18 14:04:50 -0500

    forgot some files

--- js/delta.js
+++ js/delta.js
@@ -1,51 +1,41 @@
-(function ($) {
+$( document ).ready(function() {
 
  	$("img.background").delay(1000).addClass('kenBurns');
  	$(".thank").delay(1000).animate({
- 		opacity: '1'
+ 		opacity: '1',
+ 		top: 0
  	});
  	$(".support").delay(1500).animate({
-	 		opacity: '1',
-	 		top: '0'
+	 	opacity: '1',
+	 	top: '0'
   		});
 	$(".support").delay(3000).animate({
-	 		opacity: '0'
+	 	opacity: '0'
   		});
 	$(".thank").delay(3400).animate({
-	 		opacity: '0'
-  		});
-
-// w images
-   	$(".message1").delay(5000).animate({
-		opacity: '1'
-	});
-   	$(".message2").delay(5500).animate({
-		opacity: '1'
-	});
-   	$(".message3").delay(6000).animate({
-		opacity: '1'
-	});
-   	$(".message4").delay(6500).animate({
-		opacity: '1'
-	});
-// using fonts		   	
+	 	opacity: '0'
+  		});	   	
 	$(".why1").delay(5000).animate({
-		opacity: '1'
+		opacity: '1',
+		top: 0
 	});
    	$(".why2").delay(5500).animate({
-		opacity: '1'
+		opacity: '1',
+		top: 0
 	});
    	$(".why3").delay(6000).animate({
-		opacity: '1'
+		opacity: '1',
+		top: 0
 	});
    	$(".why4").delay(6500).animate({
-		opacity: '1'
+		opacity: '1',
+		top: 0
  	});
   
  	$("img.logo").delay(6700).queue(function() { $(this).addClass('logoMove') });
  	$("a.cta").delay(8000).queue(function() { $(this).addClass('ctaMove') });
 
 
-}(jQuery));
+});
 
 
