7bd279b3899a9f485ca4cf9489f4a6786fe00357
david.pate <david@davidpate.com>
2015-11-19 10:46:29 -0500

    convert fonts to svg

--- js/delta.js
+++ js/delta.js
@@ -1,33 +1,33 @@
 $( document ).ready(function() {
 
  	$("img.background").delay(1000).addClass('kenBurns');
- 	$(".thank").delay(1000).animate({
+ 	$(".head").delay(1000).animate({
  		opacity: '1',
  		top: 0
  	});
- 	$(".support").delay(1500).animate({
+ 	$(".subhead").delay(1500).animate({
 	 	opacity: '1',
-	 	top: '0'
+	 	top: '57px'
   		});
-	$(".support").delay(3000).animate({
+	$(".subhead").delay(3000).animate({
 	 	opacity: '0'
   		});
-	$(".thank").delay(3400).animate({
+	$(".head").delay(3400).animate({
 	 	opacity: '0'
   		});	   	
-	$(".why1").delay(5000).animate({
+	$(".text1").delay(5000).animate({
+		opacity: '1',
+		top: '10px'
+	});
+   	$(".text2").delay(5500).animate({
 		opacity: '1',
 		top: 0
 	});
-   	$(".why2").delay(5500).animate({
+   	$(".text3").delay(6000).animate({
 		opacity: '1',
 		top: 0
 	});
-   	$(".why3").delay(6000).animate({
-		opacity: '1',
-		top: 0
-	});
-   	$(".why4").delay(6500).animate({
+   	$(".text4").delay(6500).animate({
 		opacity: '1',
 		top: 0
  	});
